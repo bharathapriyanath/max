6d0d5af3cd8271fe4de802db17ec2c44f238b91d
LEGEND-SAMMA <90963123+bharathapriyanath@users.noreply.github.com>
2021-12-23 17:47:07 +0530

    👑MAX👑

---
+++ max.js
@@ -0,0 +1,24 @@
+/* Copyright (C) 2021 KAVIYAAH - Alexa Team  ,  Lusifar whatsapp bot owner
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+kaviyaah - kavishka sandaruwan (v 8.0.0 avalable)
+*/
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// PLK Special Functions
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+ OB_NAME: process.env.Z_BOT_NAME === undefined ? 'bharathapriyanath' : process.env.Z_BOT_NAME,
+ OA_NAME: process.env.Z_DEP_NAME === undefined ? 'bharathapriyanath' : process.env.Z_DEP_NAME,
+ REMOVE2: process.env.THERI_KICK_PM === undefined ? 'false' : process.env.THERI_KICK_PM,
+ PLKAFN: process.env.THERI_LIST_PM === undefined ? false : process.env.THERI_LIST_PM,
+ OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'you typed kavishka , he is my creator' : process.env.Z_DEP_REPLY,
+ PHONE: process.env.NUMBER === undefined ? '+94767139407' : process.env.NUMBER,   
+    
+
+};
